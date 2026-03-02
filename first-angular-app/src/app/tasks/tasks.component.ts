f44fd005c671703ac5874f5fd6750f827c07737d
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-02 15:07:22 +0100

    50. More Component Communication: Deleting Tasks

---
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+import { required } from '@angular/forms/signals';
+import { Task } from './task/task.model';
+import { TaskComponent } from './task/task.component';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  //@Input() name?: string;
+  //@Input() name: string | undefined;
+
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary: 'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
---
+++ first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, Input, input, Output, output, EventEmitter } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserCompenent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `users/${this.user.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
