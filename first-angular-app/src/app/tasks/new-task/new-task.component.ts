8af63dbf218bc3c142c94c4de10149fd67abed40
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-03 19:03:18 +0100

    54. Signals & Two-Way-Binding

--- first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,9 +9,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDate = '';
+  // enteredTitle = '';
+  // enteredSummary = '';
+  // enteredDate = '';
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
 
   onCancel() {
     this.cancel.emit();
