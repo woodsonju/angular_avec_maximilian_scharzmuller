1a8ef546c213a808706beb0184afa3beb0b7b208
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-03 19:58:46 +0100

    57. Content Projection with ng-content

--- first-angular-app/src/app/tasks/task/task.component.ts
+++ first-angular-app/src/app/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { required } from '@angular/forms/signals';
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
