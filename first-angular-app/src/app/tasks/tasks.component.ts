9008477307c609b1ba5c3fa1e8ec19696364bbbe
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-04 11:38:50 +0100

    59. Getting Started with Services

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -19,34 +19,11 @@ export class TasksComponent {
 
   isAddingTask = false; //Par défaut, on le l'affiche pas
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
-
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return;
   }
 
+  //Remove the task with the given id from the list of tasks
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
@@ -60,14 +37,6 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    const newTask: Task = {
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.dueDate,
-    };
-    this.tasks.unshift(newTask);
     this.isAddingTask = false;
   }
 }
