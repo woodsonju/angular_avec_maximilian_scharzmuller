890be7ab8a0a539608eac298d384de6278bd6d38
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-06 11:33:31 +0100

    86. Formatting Output with a Pipe

--- 04-essentials-practice/src/main.ts
+++ 04-essentials-practice/src/main.ts
@@ -1,6 +1,11 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+import localeFr from '@angular/common/locales/fr';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideZoneChangeDetection()],
+}).catch((err) => console.error(err));
