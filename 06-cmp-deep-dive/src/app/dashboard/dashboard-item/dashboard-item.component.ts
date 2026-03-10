4a138d1dabf7823a2d52e807f5c86b6620f5149d
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-10 17:00:15 +0100

    101. Component Inputs: Repetition

--- 06-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
 
+  //Signal
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
