57f93173c81e9d48304cae8dded08e4a70b5edf5
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-10 18:17:00 +0100

    Lecture 104. Adding Forms to Components

--- 06-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -10,7 +10,7 @@ export class DashboardItemComponent {
   // @Input({ required: true }) image!: { src: string; alt: string };
   // @Input({ required: true }) title!: string;
 
-  //Signal
+  //Signal : input functions
   image = input.required<{ src: string; alt: string }>();
   title = input.required<string>();
 }
--- 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {
-
-}
+export class TicketsComponent {}
