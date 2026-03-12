dde06e40035fa9124785ca87f6f7d953b04375a7
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-12 14:57:35 +0100

    108. Exploring Advanced Content Projection

---
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+
+@Component({
+  selector: 'app-new-ticket',
+  imports: [ButtonComponent],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent {}
