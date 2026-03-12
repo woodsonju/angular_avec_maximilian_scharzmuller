2b92d7a922cdf7011b8c662e57d0cbd9d4c23d6d
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-12 16:18:50 +0100

    110. Multi-Element Custom Components & Content Projection

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
