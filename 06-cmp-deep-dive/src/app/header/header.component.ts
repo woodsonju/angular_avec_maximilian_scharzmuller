c1d3ae7d5710da301cd3986863abd38fc74d353f
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-13 00:49:56 +0100

    112. Understanding & Configuring View Encapsulation

--- 06-cmp-deep-dive/src/app/header/header.component.ts
+++ 06-cmp-deep-dive/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../shared/button/button.component';
+import { NgClass } from '../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
   selector: 'app-header',
