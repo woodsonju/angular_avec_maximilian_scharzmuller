62e897d695ecf960a45cc1f12101850530be0712
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-16 16:23:45 +0100

    117. Interacting with Host Elements via @HostListener & @HostBinding

---
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -0,0 +1,30 @@
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
+})
+export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+  label = input.required<string>();
+
+  onClick() {
+    console.log('Clicked!');
+  }
+}
