47d85d52c8e7c5a35efddba72a36d281d9d4c20c
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-20 17:50:26 +0100

    140. TypeScript & Type Models: Repetition

--- 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-tickets',
@@ -7,4 +8,6 @@ import { NewTicketComponent } from './new-ticket/new-ticket.component';
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+}
