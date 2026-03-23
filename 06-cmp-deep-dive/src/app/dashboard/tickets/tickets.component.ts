9b9dd00e9ec8710006c7a435272e0f683bf92db2
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-23 15:10:02 +0100

    142. A Closer Look At Template For Loops

--- 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
