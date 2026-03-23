937f6dbec5225d8c4fbfe39e55d0499ff7562a76
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-23 16:58:23 +0100

    145. Cross-Component Communication & State Management

--- 06-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Ticket } from '../ticket.model';
 
 @Component({
@@ -10,9 +10,14 @@ import { Ticket } from '../ticket.model';
 export class TicketComponent {
   data = input.required<Ticket>();
   detailsVisible = signal(false);
+  close = output();
 
   onToggleDetails() {
     //this.detailsVisible.set(!this.detailsVisible());
     this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
   }
 }
