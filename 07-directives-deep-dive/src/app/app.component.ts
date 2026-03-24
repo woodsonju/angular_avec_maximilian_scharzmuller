29dcf6e0ebef37de6cf34b75bb7cfa9bd7ebb5c2
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-24 16:54:06 +0100

    159. Building Another Directive

--- 07-directives-deep-dive/src/app/app.component.ts
+++ 07-directives-deep-dive/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, computed, inject } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
