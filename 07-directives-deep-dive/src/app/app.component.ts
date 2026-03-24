b3d9b60bedf0a0e5860e665cea7d4fc31b0888ec
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-24 12:42:50 +0100

    154. Analyzing a Built-in Structural Directive: ngIf

--- 07-directives-deep-dive/src/app/app.component.ts
+++ 07-directives-deep-dive/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [AuthComponent, LearningResourcesComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  private authService = inject(AuthService);
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+}
