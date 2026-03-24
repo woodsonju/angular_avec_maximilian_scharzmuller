43e18f37d641ba9ac1564ef3c597c78b7b18d95f
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-24 10:38:06 +0100

    backup before angular upgrade

--- 07-directives-deep-dive/src/app/app.component.ts
+++ 07-directives-deep-dive/src/app/app.component.ts
@@ -4,10 +4,9 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [AuthComponent, LearningResourcesComponent]
 })
 export class AppComponent {}
--- 07-directives-deep-dive/src/app/auth/auth.component.ts
+++ 07-directives-deep-dive/src/app/auth/auth.component.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css',
+    selector: 'app-auth',
+    imports: [FormsModule],
+    templateUrl: './auth.component.html',
+    styleUrl: './auth.component.css'
 })
 export class AuthComponent {
   email = signal('');
