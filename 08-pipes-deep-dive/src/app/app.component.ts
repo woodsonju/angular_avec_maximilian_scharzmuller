60e5d49ffb0da3ed646ebf8336fddf93d82e6b4f
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-26 14:46:31 +0100

    172. Understanding How Pipes Are Executed

--- 08-pipes-deep-dive/src/app/app.component.ts
+++ 08-pipes-deep-dive/src/app/app.component.ts
@@ -26,6 +26,9 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    // this.historicTemperatures[index] = 18;
+    const newTemps = [...this.historicTemperatures];
+    newTemps[index] = 18;
+    this.historicTemperatures = newTemps;
   }
 }
