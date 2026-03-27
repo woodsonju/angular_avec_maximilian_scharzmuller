fcbd7d823efadee38a77dfb70808aed04e3a78e2
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-27 16:13:07 +0100

    177. Creating a Service et 178. How NOT To Provide A Service

--- 09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
+++ 09-services-deep-dive/src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,24 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
-    selector: 'app-new-task',
-    imports: [FormsModule],
-    templateUrl: './new-task.component.html',
-    styleUrl: './new-task.component.css'
+  selector: 'app-new-task',
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  private tasksService: TasksService;
+
+  constructor() {
+    this.tasksService = new TasksService();
+  }
+
   onAddTask(title: string, description: string) {
+    this.tasksService.addTask({ title, description });
     this.formEl()?.nativeElement.reset();
   }
 }
