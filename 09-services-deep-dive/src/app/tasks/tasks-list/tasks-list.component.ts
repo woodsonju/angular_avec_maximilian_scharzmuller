fc32624c1feab89c2bca62f8926ad226e7362da5
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-27 16:42:28 +0100

    180. Using The Alternative Dependency Injection Syntax

--- 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 09-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
-    selector: 'app-tasks-list',
-    templateUrl: './tasks-list.component.html',
-    styleUrl: './tasks-list.component.css',
-    imports: [TaskItemComponent]
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
 })
 export class TasksListComponent {
+  private tasksService = inject(TasksService);
+
   selectedFilter = signal<string>('all');
-  tasks = [];
+
+  tasks = this.tasksService.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
