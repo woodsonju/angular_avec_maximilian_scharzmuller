0bf592908f0942659c9d9dbb11e61d5e776889fc
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-30 20:52:54 +0200

    184. Providing Services via the Element Injector

--- 09-services-deep-dive/src/app/tasks/tasks.component.ts
+++ 09-services-deep-dive/src/app/tasks/tasks.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksService } from './tasks.service';
 
 @Component({
-    selector: 'app-tasks',
-    templateUrl: './tasks.component.html',
-    imports: [NewTaskComponent, TasksListComponent]
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  imports: [NewTaskComponent, TasksListComponent],
+  providers: [TasksService],
 })
 export class TasksComponent {}
--- 09-services-deep-dive/src/main.ts
+++ 09-services-deep-dive/src/main.ts
@@ -2,8 +2,12 @@ import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { TasksService } from './app/tasks/tasks.service';
+// import { TasksService } from './app/tasks/tasks.service';
+
+// bootstrapApplication(AppComponent, {
+//   providers: [provideZoneChangeDetection(), TasksService],
+// }).catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection(), TasksService],
+  providers: [provideZoneChangeDetection()],
 }).catch((err) => console.error(err));
