e143f96790aadb8c307641fbf98b41c4d1b37ee7
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-30 20:41:23 +0200

    183. There Are Multiple Ways Of Providing a Service

--- 09-services-deep-dive/src/main.ts
+++ 09-services-deep-dive/src/main.ts
@@ -1,6 +1,9 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksService } from './app/tasks/tasks.service';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideZoneChangeDetection(), TasksService],
+}).catch((err) => console.error(err));
