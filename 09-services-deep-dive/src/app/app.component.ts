68e7b5b75131170ddec1d6e3e4741039b8af7f9d
Woodson JUSTE <j.woodson@hotmail.fr>
2026-03-31 11:00:50 +0200

    186. Injecting Services Into Services

--- 09-services-deep-dive/src/app/app.component.ts
+++ 09-services-deep-dive/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 
 import { TasksComponent } from './tasks/tasks.component';
 import { TasksService } from './tasks/tasks.service';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [TasksComponent],
+  //   providers: [LoggingService],
 })
 export class AppComponent {}
