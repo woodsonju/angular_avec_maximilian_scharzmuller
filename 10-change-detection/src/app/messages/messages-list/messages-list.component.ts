d8345b79e52f30a8300846e5658534467a534bc1
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-03 17:32:58 +0200

    205. Introducing The async Pipe

--- 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -8,41 +8,20 @@ import {
   OnInit,
 } from '@angular/core';
 import { MessagesService } from '../messages.services';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
+  imports: [AsyncPipe],
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messsagesService = inject(MessagesService);
-  //ChangeDetectorRef est utilisé pour déclencher manuellement la détection de changement
-  private cdRef = inject(ChangeDetectorRef);
 
-  //DestroyRef est utilisé pour gérer la durée de vie des abonnements
-  //et éviter les fuites de mémoire
-  private destroyRef = inject(DestroyRef);
-
-  messages: string[] = [];
-
-  ngOnInit(): void {
-    const subscription = this.messsagesService.message$.subscribe(
-      (messages) => {
-        this.messages = messages;
-        //markForCheck dit à OnPush de re-rendre le composant
-        //Permet déclencher manuellement la détection des changements dans un composant
-        this.cdRef.markForCheck();
-      },
-    );
-
-    //S'assure que la subscription est nettoyée lorsque le composant est supprimé,
-    //évitant ainsi les fuites de mémoire.
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+  messages$ = this.messsagesService.message$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
