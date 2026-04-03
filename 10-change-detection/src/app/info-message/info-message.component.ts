0880f7055252706dad5f426f2bcbdfc9e9c8396e
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-03 12:06:19 +0200

    196. Change Detection During Development: ExpressionChangedAfterChecked Errors

--- 10-change-detection/src/app/info-message/info-message.component.ts
+++ 10-change-detection/src/app/info-message/info-message.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-info-message',
-    imports: [],
-    templateUrl: './info-message.component.html',
-    styleUrl: './info-message.component.css'
+  selector: 'app-info-message',
+  imports: [],
+  templateUrl: './info-message.component.html',
+  styleUrl: './info-message.component.css',
 })
 export class InfoMessageComponent {
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.');
-    return 'InfoMessage Component Debug Output';
+    return Math.random();
   }
 
   onLog() {
