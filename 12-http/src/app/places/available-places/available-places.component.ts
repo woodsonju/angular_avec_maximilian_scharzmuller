7f5a1abf166e2036746dd5095f3b8382b577f7cc
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-15 17:34:42 +0200

    224. Sending a GET Request To Fetch Data

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -1,15 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-    selector: 'app-available-places',
-    templateUrl: './available-places.component.html',
-    styleUrl: './available-places.component.css',
-    imports: [PlacesComponent, PlacesContainerComponent]
+  selector: 'app-available-places',
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  //constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (resData) => {
+          console.log(resData.places);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
