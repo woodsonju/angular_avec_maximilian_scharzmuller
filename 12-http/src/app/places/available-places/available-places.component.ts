31069e7a58220b82c86b1e11f907b629fa966a96
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-15 20:16:42 +0200

    227. Showing a Loading Fallback

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,17 +14,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   //constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
       .subscribe({
-        next: (resData) => {
-          console.log(resData.places);
+        next: (places) => {
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
 
