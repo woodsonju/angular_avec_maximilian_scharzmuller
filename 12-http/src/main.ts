ce1690cae3e8315665a87dcc1f3c1e426d5ba9a6
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-17 18:56:22 +0200

    238. Introducing HTTP Interceptors

--- 12-http/src/main.ts
+++ 12-http/src/main.ts
@@ -1,7 +1,25 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+) {
+  console.log('Outgoing Request');
+  console.log(request);
+  //   const req = request.clone({
+  //     headers: request.headers.set('X-DEBUG', 'TESTING'),
+  //   });
+  return next(req);
+}
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
+}).catch((err) => console.error(err));
