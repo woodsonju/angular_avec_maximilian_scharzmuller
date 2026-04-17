d2150a98dde6261a478643fa24734faf9a204c83
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-17 17:04:14 +0200

    232. Managing HTTP-loaded Data via a Service and 233. Implementing Optimistic Updating

--- 12-http/src/app/places/places.service.ts
+++ 12-http/src/app/places/places.service.ts
@@ -2,7 +2,7 @@ import { DestroyRef, inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -26,12 +26,19 @@ export class PlacesService {
     return this.fetchPlaces(
       'http://localhost:3000/user-places',
       'Something went wrong fetching your favorite places. Please try again later.',
+    ).pipe(
+      tap({
+        next: (userPlaces) => {
+          this.userPlaces.set(userPlaces);
+        },
+      }),
     );
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    this.userPlaces.update((prevPlaces) => [...prevPlaces, place]);
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId,
+      placeId: place.id,
     });
   }
 
--- 12-http/src/app/places/user-places/user-places.component.ts
+++ 12-http/src/app/places/user-places/user-places.component.ts
@@ -12,20 +12,17 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
 
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
 
+  places = this.placesService.loadedUserPlaces;
+
   ngOnInit(): void {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        this.places.set(places);
-      },
-
       error: (err: Error) => {
         this.error.set(err.message);
       },
