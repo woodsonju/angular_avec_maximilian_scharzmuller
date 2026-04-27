d4604f62e31c5b4ad6ee9a9984751f718592bd92
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-27 15:30:29 +0200

    backup before angular upgrade

--- 13-forms/src/app/app.component.ts
+++ 13-forms/src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './auth/login/login.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [LoginComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    imports: [LoginComponent]
 })
 export class AppComponent {}
