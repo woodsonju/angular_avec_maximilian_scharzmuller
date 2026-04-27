3320ac9865933521b43a423d50e134cf79c85641
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-27 15:55:45 +0200

    243. Template-driven: Registering Form Controls

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
