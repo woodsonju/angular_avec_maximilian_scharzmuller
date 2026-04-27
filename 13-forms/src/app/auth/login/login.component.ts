c01e1d7893f0678a6022cd6dda374e63e2655a87
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-27 18:09:40 +0200

    249. Interacting With The Underlying Form Object In The Component

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { form } from '@angular/forms/signals';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +17,28 @@ import { form } from '@angular/forms/signals';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'save-login-form',
+              JSON.stringify({
+                email: value.email,
+              }),
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
       return;
@@ -19,5 +48,7 @@ export class LoginComponent {
     const enteredPassword = formData.form.value.password;
     console.log(formData.form);
     console.log(enteredEmail, enteredPassword);
+
+    formData.reset();
   }
 }
