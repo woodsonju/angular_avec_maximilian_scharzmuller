9e5de8d4e812b906b719f12953f7254ff405286e
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-28 16:55:30 +0200

    268. Enabling Routing & Adding a First Route

--- 14-routing/src/main.ts
+++ 14-routing/src/main.ts
@@ -1,6 +1,12 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
