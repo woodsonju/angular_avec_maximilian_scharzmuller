d79ca780eb914d0cc36b628e21b2282efc9b3d69
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-28 19:02:33 +0200

    271. Adding Links The Right Way

--- 14-routing/src/app/users/users.component.ts
+++ 14-routing/src/app/users/users.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 
 import { UserComponent } from './user/user.component';
 import { UsersService } from './users.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-users',
-    templateUrl: './users.component.html',
-    styleUrl: './users.component.css',
-    imports: [UserComponent]
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+  imports: [UserComponent],
 })
 export class UsersComponent {
   private usersService = inject(UsersService);
