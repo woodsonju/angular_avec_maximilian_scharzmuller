37e9cdf12578d040a62415e7578cd16b10dd78ef
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-28 13:11:54 +0200

    260. Connecting & Registering Inputs For A Complex Form

--- 13-forms/src/app/auth/signup/signup.component.ts
+++ 13-forms/src/app/auth/signup/signup.component.ts
@@ -21,6 +21,35 @@ export class SignupComponent {
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)],
     }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', {
+      validators: [Validators.required],
+    }),
+    agree: new FormControl(false, {
+      validators: [Validators.required],
+    }),
   });
 
   get emailIsInvalid() {
