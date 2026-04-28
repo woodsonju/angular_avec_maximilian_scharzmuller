4ad2bf55399f94499de26112522e57f665998a77
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-28 19:48:40 +0200

    274. Extracting Dynamic Route Parameters via Inputs

--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  //voir app.config.ts : withComponentInputBinding() permet de lier
+  //automatiquement les paramètres de l'URL (:userId). Plus besoin d'ActivatedRoute(ancienne méthode)
+  userId = input.required<string>();
+
+  private userService = inject(UsersService);
+
+  userName = computed(
+    () => this.userService.users.find((u) => u.id === this.userId())?.name,
+  );
+}
