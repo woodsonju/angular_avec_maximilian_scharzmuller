50e17e43316d54b6282d7041b5b71d09401b5a67
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-29 16:23:55 +0200

    288. Extracting Query Parameters via Observables

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,9 +1,16 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -12,13 +19,23 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   //Avec l' tilisation de  withComponentInputBinding() et withRouterConfig
   // =>  reçoit automatiquement :userId de la route PARENT
-  //
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+
+  // reçoit automatiquement ?order=asc depuis l'URL
+  // grâce à withComponentInputBinding() ✅
+  //
+  // Pas de required → peut être undefined ✅
+  // (premier chargement sans query param)
+  //order = input<'asc' | 'desc'>();
+
+  //Via activatedRoute
+  order?: 'asc' | 'desc';
+
   private tasksService = inject(TasksService);
+  private destroyRef = inject(DestroyRef);
 
   //Calculé automatiquement
   userTasks = computed(() =>
@@ -26,4 +43,14 @@ export class TasksComponent {
       .allTasks()
       .filter((task) => task.userId === this.userId()),
   );
+
+  //Via activatedRoute
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
