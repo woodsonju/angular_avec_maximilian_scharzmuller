4ad24d926fa4aa2488ce9f4487122a5d81d94fb2
Woodson JUSTE <j.woodson@hotmail.fr>
2026-04-30 11:29:35 +0200

    300. Reloading Pages via the Angular Router & Configuring Programmatic Navigation

--- 14-routing/src/app/tasks/task/task.component.ts
+++ 14-routing/src/app/tasks/task/task.component.ts
@@ -4,6 +4,7 @@ import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-task',
@@ -16,7 +17,16 @@ export class TaskComponent {
   task = input.required<Task>();
   private tasksService = inject(TasksService);
 
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+
   onComplete() {
     this.tasksService.removeTask(this.task().id);
+    this.router.navigate(['./'], {
+      //navigation relative à la route courante
+      relativeTo: this.activatedRoute,
+      onSameUrlNavigation: 'reload', //recharger la même URL pour mettre à jour la liste des tâches après suppression
+      queryParamsHandling: 'preserve', //conserver les query params (ex: ?order=asc) après suppression
+    });
   }
 }
--- 14-routing/src/app/users/users.routes.ts
+++ 14-routing/src/app/users/users.routes.ts
@@ -22,7 +22,7 @@ export const routes: Routes = [
   {
     path: 'tasks', //<your-domain>/users/<uid>/tasks
     component: TasksComponent,
-    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
     },
